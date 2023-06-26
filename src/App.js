bdad34d525b46fdc0405c58dff9dc8f52f4478ed
Lars Andersen <expez@expez.com>
2023-06-26 09:53:02 +0200

    Add error message when mic permissions isn't available

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import PitchDetector from "./PitchDetector";
 import { NoteProvider } from "./NoteContext";
 import { QuestionProvider } from './QuestionContext';
 import Question from './Question';
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+ const [micPermission, setMicPermission] = useState(false);
+
+ useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+      .then(() => setMicPermission(true))
+      .catch(() => setMicPermission(false));
+  }, []); 
+
+  if (!micPermission) {
+    return (
+      <div className="app">
+        <div className="error-message">Microphone permission not granted.</div>
+      </div>
+    );
+  }
+
   return (
     <div className='app'>
       <QuestionProvider>
